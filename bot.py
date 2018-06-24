f0884b62885d404868928175973b551e0b61114b
Aleksandr Petrov <petrov.aa@phystech.edu>
2018-06-24 10:47:38 +0300

    remove port local port listening

--- bot.py
+++ bot.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
 
         external_ip = bot_config['webhook_host']
         external_port = int(bot_config['webhook_port'])
-        listen = '127.0.0.1'
 
         ssl_cert = bot_config['public_cert_path']
 
@@ -42,7 +41,6 @@ if __name__ == '__main__':
 
         web.run_app(
             app,
-            host=listen,
             path=bot_config['socket_path']
         )
 
